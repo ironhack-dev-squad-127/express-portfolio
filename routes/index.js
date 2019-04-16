a6c5e0b08481f7b567e65d26e00ab25af13c6680
diogo <diogobessac@gmail.com>
2019-04-16 12:55:42 +0100

    diogo

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,9 @@ router.get('/ironhackers/maxence', (req, res, next) => {
   res.render('ironhackers/maxence');
 });
 
+router.get('/ironhackers/diogo', (req, res, next) => {
+  // Render "/views/ironhackers/diogo.hbs"
+  res.render('ironhackers/diogo');
+});
+
 module.exports = router;
